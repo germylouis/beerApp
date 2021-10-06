de2146de1a929ccec97e1db882f9fa67179fa645
Germy Louis <glouis@imobile3.com>
2021-10-06 13:28:30 -0400

    Refactor file structure.

--- app/src/main/java/com/example/beerapp/ui/MainActivity.kt
+++ app/src/main/java/com/example/beerapp/ui/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.beerapp
+package com.example.beerapp.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.beerapp.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
