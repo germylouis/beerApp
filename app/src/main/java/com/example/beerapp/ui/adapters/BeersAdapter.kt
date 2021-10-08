a817f09945a824a0264194266ca66dd7b98754ef
Germy Louis <glouis@imobile3.com>
2021-10-07 23:56:30 -0400

    Set theme and add DataBinding with Glide.

--- app/src/main/java/com/example/beerapp/ui/adapters/BeersAdapter.kt
+++ app/src/main/java/com/example/beerapp/ui/adapters/BeersAdapter.kt
@@ -2,7 +2,12 @@ package com.example.beerapp.ui.adapters
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import android.widget.ImageView
+import androidx.databinding.BindingAdapter
 import androidx.recyclerview.widget.RecyclerView
+import androidx.swiperefreshlayout.widget.CircularProgressDrawable
+import com.bumptech.glide.Glide
+import com.example.beerapp.R
 import com.example.beerapp.data.entities.Beer
 import com.example.beerapp.databinding.BeersListViewBinding
 
@@ -21,10 +26,25 @@ class BeersAdapter(private val beers: List<Beer>) :
 
     override fun getItemCount(): Int = beers.size
 
-    inner class BeersViewHolder(val binding: BeersListViewBinding) :
+    inner class BeersViewHolder(private val binding: BeersListViewBinding) :
         RecyclerView.ViewHolder(binding.root) {
         fun bind(beer: Beer) {
             binding.listItem = beer
+        }
+    }
+
+    companion object {
+        @JvmStatic
+        @BindingAdapter("loadImage")
+        fun loadImage(view: ImageView, url: String) {
+            val circularProgressDrawable = CircularProgressDrawable(view.context)
+            circularProgressDrawable.strokeWidth = 5f
+            circularProgressDrawable.centerRadius = 30f
+            circularProgressDrawable.start()
+            Glide.with(view).asBitmap().load(url).centerCrop()
+                .placeholder(circularProgressDrawable)
+                .error(R.drawable.ic_launcher_foreground)
+                .fallback(R.drawable.ic_launcher_foreground).into(view)
         }
     }
 }
